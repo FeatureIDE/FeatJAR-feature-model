569bee222607ef96aa1ab394605f1825c590afb5
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2024-05-23 13:54:45 +0200

    Applies reformatting

--- src/main/java/de/featjar/feature/project/Project.java
+++ src/main/java/de/featjar/feature/project/Project.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright (C) 2024 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-feature-model.
+ *
+ * feature-model is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * feature-model is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with feature-model. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-feature-model> for further information.
+ */
 package de.featjar.feature.project;
 
 import de.featjar.feature.model.FeatureModel;
-
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,7 +31,6 @@ import java.util.List;
  *
  * @author Andreas Gerasimow
  */
-
 public class Project {
 
     public static class ConfigurationPath {
@@ -44,7 +62,8 @@ public class Project {
         this.configurationPaths = new ArrayList<>();
     }
 
-    public Project(HashMap<String, Object> metaData, FeatureModel featureModel, List<ConfigurationPath> configurationPaths) {
+    public Project(
+            HashMap<String, Object> metaData, FeatureModel featureModel, List<ConfigurationPath> configurationPaths) {
         this.metaData = metaData;
         this.featureModel = featureModel;
         this.configurationPaths = configurationPaths;
