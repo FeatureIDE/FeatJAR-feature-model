a67f8e59d4c1cbafdfd9103a9a6e6206ccea7dfc
Praktikum Gruppe 10 <gruppe10@wks10.isf.cs.tu-bs.de>
2025-10-02 15:21:55 +0200

    feat: further implementation of features from story card WIP

--- src/main/java/de/featjar/feature/model/cli/PrintStatistics.java
+++ src/main/java/de/featjar/feature/model/cli/PrintStatistics.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2025 FeatJAR-Development-Team
  *
- * This file is part of FeatJAR-formula.
+ * This file is part of FeatJAR-feature-model.
  *
- * formula is free software: you can redistribute it and/or modify it
+ * feature-model is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * formula is distributed in the hope that it will be useful,
+ * feature-model is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with formula. If not, see <https://www.gnu.org/licenses/>.
+ * along with feature-model. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-feature-model> for further information.
  */
 package de.featjar.feature.model.cli;
 
@@ -26,24 +26,12 @@ import de.featjar.base.cli.Option;
 import de.featjar.base.cli.OptionList;
 import de.featjar.base.data.Result;
 import de.featjar.base.io.IO;
-import de.featjar.base.io.csv.CSVFile;
-import de.featjar.base.tree.Trees;
-import de.featjar.formula.cli.PrintCommand.WhitespaceString;
-import de.featjar.formula.io.FormulaFormats;
-import de.featjar.formula.io.textual.ExpressionSerializer;
-import de.featjar.formula.io.textual.Symbols;
-import de.featjar.formula.structure.IFormula;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import de.featjar.feature.model.IFeatureModel;
+import de.featjar.feature.model.io.FeatureModelFormats;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Locale;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.HashMap;
-
 
 /**
  * Prints statistics about a provided Feature Model.
@@ -51,36 +39,90 @@ import java.util.HashMap;
  * @author Knut & Kilian
  */
 public class PrintStatistics extends ACommand {
-	
-    public static final Option<Boolean> PRETTY_PRINT = Option.newFlag("print")
-            .setDescription("Pretty prints the numbers");
-    
-    public static final Option<Path> INPUT_OPTION = Option.newOption("path", Option.PathParser)
-            .setDescription("Path to input file(s)");
-    	 // .setValidator(Option.PathValidator)
-    
-	private HashMap<String, Integer> data = new HashMap<String, Integer>();
-	
+
+    enum FileTypes {
+        XML,
+        CSV,
+        YAML,
+        JSON
+    }
+
+    enum AnalysesScope {
+        ALL,
+        TREE_RELATED,
+        CONSTRAINT_RELATED
+    }
+
+    public static final Option<FileTypes> FILE_TYPE =
+            Option.newEnumOption("type", FileTypes.class).setDescription("Specifies file type");
+
+    public static final Option<AnalysesScope> ANALYSES_SCOPE =
+            Option.newEnumOption("scope", AnalysesScope.class).setDescription("Specifies scope of statistics");
+
+    public static final Option<Boolean> PRETTY_PRINT =
+            Option.newFlag("pretty").setDescription("Pretty prints the numbers");
+
+    private HashMap<String, Integer> data;
+    private FileTypes type;
+
     @Override
     public int run(OptionList optionParser) {
-    	Boolean bool_prettyPrint = optionParser.get(PRETTY_PRINT);
-    	Path path = optionParser.get(INPUT_OPTION);
-    	
-    	
-    	if (!path.equals(null)) {
-        	FeatJAR.log().message(path);
-    	}
-    	if(bool_prettyPrint) {
-    		FeatJAR.log().message("PRETTY");
-    	}
-    	
-    	FeatJAR.log().message("STATISTICS ABOUT THE FEATURE MODEL:\n" + messageLog());
-        
+
+        // -----------------INPUT--------------------------------------------------
+
+        // temporary dummy model
+        // Path path = Paths.get("../formula/src/testFixtures/resources/Automotive02_V1/model.xml");
+
+        Path path = optionParser.getResult(INPUT_OPTION).orElseThrow();
+        Result<IFeatureModel> load = IO.load(path, FeatureModelFormats.getInstance());
+        IFeatureModel model = load.orElseThrow();
+
+        // -----------------COLLECTING STATS--------------------------------------
+
+        data = collectStats(model);
+
+        // -----------------WRITING TO FILE---------------------------------------
+
+        if (optionParser.getResult(OUTPUT_OPTION).isPresent()) {
+            Path outputPath = optionParser.getResult(OUTPUT_OPTION).get();
+            // writeTo(outputPath);
+        }
+
+        // ----------------PRINTING IN CONSOLE------------------------------------
+
+        if (optionParser.get(PRETTY_PRINT)) {
+            printStatsPretty();
+        } else {
+            printStats();
+        }
+
         return 0;
     }
-    
-    private StringBuilder messageLog() {
-    	    	
+
+    // temporary for format type output
+    private void writeTo(Path path) {
+        switch (type) {
+            case XML:
+                // TODO future Story Card: Write to XML
+                // IO.save(new (), path, new XMLFeatureModelFormat());
+                break;
+            case CSV:
+                // TODO future Story Card: Write to CSV
+                break;
+            case YAML:
+                // TODO future Story Card: Write to YAML
+                break;
+            case JSON:
+                // TODO future Story Card: Write to JSON
+                break;
+        }
+    }
+
+    private HashMap<String, Integer> collectStats(IFeatureModel model) {
+
+        HashMap<String, Integer> data = new HashMap<String, Integer>();
+
+        // dummy values, will be handled by functions of other teams
         data.put("numOfTopFeatures", 3);
         data.put("numOfLeafFeatures", 12);
         data.put("treeDepth", 3);
@@ -89,21 +131,22 @@ public class PrintStatistics extends ACommand {
         data.put("numInAltGroups", 5);
         data.put("numOfAtoms", 8);
         data.put("avgNumOfAtomsPerConstraints", 4);
-        
+
+        return data;
+    }
+
+    public void printStatsPretty() {
         StringBuilder outputString = new StringBuilder();
-        
+
         for (Map.Entry<?, ?> entry : data.entrySet()) {
-        	outputString.append(String.format("%-30s : %s%n", entry.getKey(), entry.getValue()));
+            outputString.append(String.format("%-30s : %s%n", entry.getKey(), entry.getValue()));
         }
-    	return outputString;
+        FeatJAR.log().message("STATISTICS ABOUT THE FEATURE MODEL:\n" + outputString);
     }
-    
-    private String writeTocsv() {
-    	//Path path = new Path()
-    	//CSVFile file = new CSVFile(path);
-    	return "";
+
+    public void printStats() {
+        FeatJAR.log().message("STATISTICS ABOUT THE FEATURE MODEL:\n" + data);
     }
-    
 
     @Override
     public Optional<String> getDescription() {
