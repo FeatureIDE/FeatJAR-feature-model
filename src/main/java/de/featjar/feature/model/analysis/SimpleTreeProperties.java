c1800caf0a9eb16d60f5750f95a6c02c5074c397
Benjamin von Holt <b.von-holt@tu-braunschweig.de>
2025-10-02 16:44:18 +0200

    refactor

--- src/main/java/de/featjar/feature/model/analysis/SimpleTreeProperties.java
+++ src/main/java/de/featjar/feature/model/analysis/SimpleTreeProperties.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2025 FeatJAR-Development-Team
- *
- * This file is part of FeatJAR-feature-model.
- *
- * feature-model is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3.0 of the License,
- * or (at your option) any later version.
- *
- * feature-model is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with feature-model. If not, see <https://www.gnu.org/licenses/>.
- *
- * See <https://github.com/FeatureIDE/FeatJAR-feature-model> for further information.
- */
 package de.featjar.feature.model.analysis;
 
 import de.featjar.base.data.Result;
@@ -27,6 +7,7 @@ import de.featjar.feature.model.*;
 import de.featjar.feature.model.analysis.visitor.FeatureTreeGroupCounter;
 import de.featjar.feature.model.analysis.visitor.TreeAvgChildrenCounter;
 import de.featjar.feature.model.analysis.visitor.TreeLeafCounter;
+
 import java.util.HashMap;
 
 public class SimpleTreeProperties {
@@ -72,4 +53,5 @@ public class SimpleTreeProperties {
     public Result<HashMap<String, Integer>> groupDistribution(IFeatureTree tree) {
         return Trees.traverse(tree, new FeatureTreeGroupCounter());
     }
+
 }
