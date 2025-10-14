c0d401f022946de7b44b90951932a19a17c6bf0d
Benjamin von Holt <b.von-holt@tu-braunschweig.de>
2025-10-14 16:45:16 +0200

    feat: Analysis Tree now integrated into chart building pipeline + built first chart to display group distribution

--- src/main/java/de/featjar/feature/model/analysis/visualization/VisualizeFeatureGroupDistribution.java
+++ src/main/java/de/featjar/feature/model/analysis/visualization/VisualizeFeatureGroupDistribution.java
@@ -1,7 +1,14 @@
 package de.featjar.feature.model.analysis.visualization;
 
 import de.featjar.feature.model.analysis.AnalysisTree;
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.PieChartBuilder;
+import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.internal.chartpart.Chart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 public class VisualizeFeatureGroupDistribution extends AVisualizeFeatureModelStats{
     public VisualizeFeatureGroupDistribution(AnalysisTree<?> analysisTree) {
@@ -14,10 +21,46 @@ public class VisualizeFeatureGroupDistribution extends AVisualizeFeatureModelSta
 
     /**
      *
-     * {@return the chart that will be used by the other class methods}
+     * {@return String key used to fetch data from the Analysis Tree later.}
+     */
+    @Override
+    protected String getAnalysisTreeDataName() {
+        return "Group Distribution";
+    }
+
+    /**
+     * You can use the analysisTreeData array list to access the analysisTree data relevant for building your chart.
+     * @return the chart that will be used by the other class methods
      */
     @Override
     Chart<?, ?> buildChart() {
-        return null;
+
+        PieChart chart1 = new PieChartBuilder().build();
+
+        // todo sort keys for trees and in alphabetical order
+        for (String key : this.analysisTreeData.keySet()) { // für jeden Baum
+            @SuppressWarnings("unchecked")
+            HashMap<String, Object> nestedMap = (HashMap<String, Object>) this.analysisTreeData.get(key);
+            Set<String> groupDistributionKeys = nestedMap.keySet();
+
+            for (String groupKey : groupDistributionKeys) {
+                ArrayList<?> groupResult = (ArrayList<?>) nestedMap.get(groupKey);
+                chart1.addSeries(groupKey, (Integer) groupResult.get(2));
+            }
+        }
+
+        // placeholder
+        /*
+        PieChart chart1 = new PieChartBuilder().build();
+        chart1.addSeries("Banananana", 40);
+        chart1.addSeries("Apfel", 25);
+        chart1.addSeries("Gurke", 50);
+
+         */
+
+        // data that we actually want to use
+        // System.out.println(this.analysisTreeData);
+
+        return chart1;
     }
 }
